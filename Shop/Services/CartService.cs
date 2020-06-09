8a79994a1875416eda583def595711fb158e2da6
Adam Kowalski <adamkowalskik44@gmail.com>
2020-06-09 19:30:58 +0200

    add cart page

--- Shop/Services/CartService.cs
+++ Shop/Services/CartService.cs
@@ -1,4 +1,5 @@
 ﻿using Shop.Data;
+using Shop.Data.ProductTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,12 @@ namespace Shop.Services
     public class CartService
     {
         private readonly ApplicationDbContext _db;
+        private readonly ProductService productService;
 
         public CartService(ApplicationDbContext db)
         {
             _db = db;
+            productService = new ProductService(_db);
         }
 
         public bool AddToCart(CartEntry cartEntry)
@@ -43,6 +46,62 @@ namespace Shop.Services
                 }
             }
             return true;
+        }
+
+        public bool DeleteCartEntry(CartEntry cartEntry)
+        {
+            try
+            {
+                _db.CartEntries.Remove(cartEntry);
+                _db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
+
+        public bool SaveCart(List<CartEntry> cartEntries)
+        {
+            foreach (var cartEntry in cartEntries)
+            {
+                try
+                {
+                    CartEntry existingEntry = _db.CartEntries.FirstOrDefault(u => u.CartEntryId == cartEntry.CartEntryId);
+                    if (existingEntry != null)
+                    {
+                        existingEntry.Quantity = cartEntry.Quantity;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            _db.SaveChanges();
+            return true;
+        }
+
+        public Dictionary<ProductDTO, CartEntry> GetUserCartProducts(string userId)
+        {
+            List<CartEntry> cartEntries = _db.CartEntries.ToList();
+            Dictionary<ProductDTO, CartEntry> result = new Dictionary<ProductDTO, CartEntry>();
+
+            foreach (var cartEntry in cartEntries)
+            {
+                if (cartEntry.UserId == userId)
+                {
+                    ProductDTO productDTO = productService.GetProductDTO(cartEntry.ProductId);
+                    if (productDTO != null)
+                    {
+                        result.Add(productDTO, cartEntry);
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
